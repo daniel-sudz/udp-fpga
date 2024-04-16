5a3cecf62037f0aa385bdb5529b9d05530bcd444
Daniel Sudzilouski <daniel@sudzilouski.com>
2024-04-16 16:48:43 -0400

    update packet parsing

--- software/src/ether.cpp
+++ software/src/ether.cpp
@@ -16,39 +16,39 @@
 #include <iostream>
 
 
-
-
 struct EtherParams {
     char DEFAULT_IF[IFNAMSIZ-1] = "enp0s31f6";
     uint16_t ETHER_TYPE_IPV4 = 0x0800;
 };
 
+/* Ethernet frames are read directly into the buffer field of this struct */ 
 struct EtherPacket {
+    // ethernet technically supports variable sized frames (jumbo)
+    // but their usage is rather rare and we can stick with fixed sized packets
+    uint8_t buff[1500];
 
-    struct ether_header ether_header_d;
-    struct iphdr ip_header_d; 
-    struct udphdr udp_header_d;
+    struct ether_header* ether_header_d = (ether_header*) buff;                       // start of packet has ether header start
+    struct iphdr* ip_header_d = (iphdr*) (ether_header_d + sizeof(ether_header));     // ip header is inside of ether header
+    
+    // since ip header can be variable in size, its location is not actually known until ip header is parsed
+    struct udphdr* udp_header_d = nullptr;
 
     struct ifreq if_ip = {};
     struct sockaddr_storage their_addr;
+};
 
-    EtherPacket(size_t packet_size) {
-     //   ether_header_d = (ether_header*) malloc(packet_size);
-     //   ip_header_d = (iphdr*) malloc(packet_size + sizeof(ether_header));
-     //   udp_header_d = (udphdr*) malloc(packet_size )
+/* Contains some human-readable metadata alongside the raw ethernet frame*/
+struct EtherPacketParsed {
+    EtherPacket packet_raw;
 
-    }
-
-    ~EtherPacket () {
-    }
+    size_t udp_payload_size;
+    uint8_t* udp_payload;
 };
 
 struct EtherPacketWatch {
     EtherParams params;
 
     struct ifreq ifopts;	
-	struct ifreq if_ip;	
-	struct sockaddr_storage their_addr;
 
     int sock_fd = -1;
     int sock_opt = 1;
@@ -90,6 +90,22 @@ struct EtherPacketWatch {
 
         // great success 
         return 0;
+    }
+
+    EtherPacketParsed read_udp() {
+        // read a UDP packet
+        EtherPacketParsed parsed;
+        int bytes_recv = recvfrom(sock_fd, parsed.packet_raw.buff, sizeof(parsed.packet_raw.buff), 0, NULL, NULL);
+
+        // payload size is the size of the whole UDP packet minux the header size
+        parsed.udp_payload_size = ntohs(parsed.packet_raw.udp_header_d->len) - sizeof(udphdr);
+
+        // the IP header is variable in size and the ihl field carries the number of 32-bit words the header contains
+        int header_size = (parsed.packet_raw.ip_header_d->ihl * 4);
+        parsed.packet_raw.udp_header_d = (udphdr*) ((uint8_t*)parsed.packet_raw.ip_header_d + header_size);
+        parsed.udp_payload = ((uint8_t*) parsed.packet_raw.udp_header_d) + sizeof(udphdr);
+        
+        return parsed;
     }
 
 };
