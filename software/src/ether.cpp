5804fbf181dcf73b3ee3a574ac792416ae4f0e0b
Daniel Sudzilouski <daniel@sudzilouski.com>
2024-04-16 13:51:32 -0400

    need ubuntu

--- software/src/ether.cpp
+++ software/src/ether.cpp
@@ -1,4 +1,27 @@
+#include <arpa/inet.h>
+#include <linux/if_packet.h>
+#include <linux/ip.h>
+#include <linux/udp.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <net/if.h>
+#include <netinet/ether.h>
+
+
+struct ethernet_header {
+
+    struct ether_header *eh;
+
+    
+    ~ethernet_header () {
+
+    }
+};
+
 
 int main() {
-    
+
 }
