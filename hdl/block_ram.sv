d1a9060878adcb2be184446acd4bbe26f68fec96
AriB2003 <49720892+AriB2003@users.noreply.github.com>
2024-04-19 00:36:20 -0400

    Buffering (#10)
    * LEDs connected to debug logic signals for PHY
    
    * Got ethernet printing over UART
    
    * Fix gitignore
    
    * First attempt at RAM
    
    * One shot Ethernet packet
    
    * Returns half of expected bitstream
    
    * Getting different packets of correct size

---
+++ hdl/block_ram.sv
@@ -0,0 +1,35 @@
+`timescale 1ns/1ps
+
+// Based on UG901 - The Vivado Synthesis Guide
+
+module block_ram(clk, rd_addr, rd_data, wr_addr, wr_ena, wr_data);
+
+parameter W = 32; // number of rows of the memory
+parameter L = 375; // Length of the memory row
+parameter INIT = "zeros.memh";
+
+input wire clk;
+input wire [$clog2(L)-1:0] wr_addr, rd_addr;
+output logic [W-1:0] rd_data;
+input wire wr_ena;
+input logic [W-1:0] wr_data;
+
+logic [W-1:0] ram [0:L-1];
+initial begin
+  $display("Initializing block ram from file %s.", INIT);
+  $readmemh(INIT, ram); // Initializes the RAM with the values in the init file.
+end
+
+always_ff @(posedge clk) begin : synthesizable_rom
+  rd_data <= ram[rd_addr];
+  if(wr_ena) begin
+    ram[wr_addr] <= wr_data;
+  end
+end
+
+task dump_memory(string file);
+  $writememh(file, ram);
+endtask
+
+
+endmodule
--- hdl/main.sv
+++ hdl/main.sv
@@ -1,8 +1,12 @@
 `timescale 1ns/1ps
 
+`timescale 1ns/1ps
+
 module main(mainclk,
 btn,
 sw,
+uart_rxd_out,
+uart_txd_in,
 uart_rxd_out,
 uart_txd_in,
 led0_b, led0_g, led0_r,
@@ -24,20 +28,87 @@ eth_tx_en,
 eth_txd //4-bit
 );
 
+// General
 input wire mainclk;     // 100 MHz
 logic [16:0] clk_divider;
 logic dataclk;           // 25 MHz
+logic uartclk;           // 12.5 MHz
 logic uartclk;           // 12.5 MHz
 input wire [3:0] btn;   // Buttons
 input wire [3:0] sw;    // Switches
 logic rst;
 
-always_comb rst = |btn;
+// Output
+input wire uart_txd_in;
+output logic uart_rxd_out;
+wire tx_ready;
+logic tx_valid;
+logic [7:0] tx_data;
+logic [31:0] read_buffer;
+
+always_comb rst = |btn[3:1];
 
 always_comb dataclk = clk_divider[1]; // 4x division
 always_comb uartclk = clk_divider[2]; // 8x division
 always_ff @(posedge mainclk) begin : Clock_Divider
     clk_divider <= clk_divider + 1;
+end
+
+//#######################        FS MACHINE        #############################
+
+enum logic [2:0] {
+    E_ERR = 0,
+    E_IDLE = 1,
+    E_REC = 2,
+    U_TX = 3
+} eth_state; 
+
+// enum logic [2:0] {
+//     U_ERR = 0,
+//     U_IDLE = 1,
+//     U_TX = 2
+// } uart_state; 
+
+logic change_state;
+logic eth_start;
+logic uart_start;
+logic one_shot;
+
+always_ff @(posedge mainclk) begin : Finite_State_Machine
+    if(rst) begin
+        eth_state<=E_IDLE;
+        one_shot<=1;
+        eth_start<=1;
+        uart_start<=1;
+        // uart_state<=U_IDLE;
+    end
+    if(eth_state==E_IDLE & change_state) begin
+        eth_state<=E_REC;
+        eth_start<=0;
+        uart_start<=1;
+        one_shot<=0;
+    end else if(eth_state==E_REC & change_state) begin
+        eth_state<=U_TX;
+        eth_start<=1;
+        uart_start<=0; 
+    end else if(eth_state==U_TX & change_state) begin
+        eth_state<=E_IDLE;
+        eth_start<=1;
+        uart_start<=1;
+    end
+    one_shot<=btn[0];
+end
+
+always_comb begin : State_Change
+    if(eth_state==E_IDLE & one_shot) begin
+        change_state=eth_rx_dv;
+    end else if(eth_state==E_REC) begin
+        change_state=~eth_rx_dv & tx_ready;
+    end else if(eth_state==U_TX) begin
+        change_state=(uart_addr>=9'd375); // TODO: add correct bound (375?)
+    end else begin
+        change_state=0;
+    end
 end
 
 //######################        ETHERNET PHY        ############################
@@ -73,25 +144,60 @@ assign eth_mdio = (1) ? 1'b1 : 1'bz; // Pulling low starts communication
 always_comb eth_rstn = ~rst; // If any button reads high
 
 always_ff @(posedge eth_rx_clk) begin : Ethernet_Receive
-    if(rst) begin
-        circle_buffer<='0;
-        write_pointer<=0;
-    end
-    if(eth_rx_dv) begin // TODO: add & not error
-        circle_buffer[write_pointer]<=eth_rxd[0];
-        circle_buffer[write_pointer+1]<=eth_rxd[1];
-        circle_buffer[write_pointer+2]<=eth_rxd[2];
-        circle_buffer[write_pointer+3]<=eth_rxd[3];
-        write_pointer <= write_pointer + 4;
+    if(rst|eth_start) begin
+        // circle_buffer<='0;
+        // write_pointer<=0;
+        eth_addr<=0;
+        wr_data<=0;
+        wr_ena<=0;
+        ethbitcounter<=0;
+    end else begin
+        if(eth_state==E_REC & eth_rx_dv) begin // TODO: add & not error
+            // circle_buffer[write_pointer]<=eth_rxd[0];
+            // circle_buffer[write_pointer+1]<=eth_rxd[1];
+            // circle_buffer[write_pointer+2]<=eth_rxd[2];
+            // circle_buffer[write_pointer+3]<=eth_rxd[3];
+            // write_pointer <= write_pointer + 4;
+            wr_data[27:0]<=wr_data[31:4];
+            wr_data[31:28]<={eth_rxd[3],eth_rxd[2],eth_rxd[1],eth_rxd[0]};
+            ethbitcounter<=ethbitcounter+1;
+            if(&ethbitcounter) begin
+                eth_addr<=eth_addr+1; // This is one addr desynced
+                wr_ena<=1;
+            end else begin
+                wr_ena<=0; 
+            end
+        end
     end
 end
 
+//#######################        BLOCK RAM?        #############################
+
+logic [8:0] addr;
+logic [8:0] eth_addr;
+logic [8:0] uart_addr;
+logic wr_ena;
+logic [31:0] wr_data;
+wire [31:0] rd_data;
+logic [2:0] ethbitcounter;
+logic [4:0] uartbitcounter;
+
+always_comb begin
+    if(eth_state==E_REC) begin
+        addr = eth_addr;
+    end else if(eth_state==U_TX) begin
+        addr = uart_addr;
+    end else begin
+        addr = 0;
+    end
+end
+
+block_ram RAM(
+  .clk(mainclk), .rd_addr(addr), .rd_data(rd_data),
+  .wr_addr(addr), .wr_ena(wr_ena), .wr_data(wr_data)
+);
+
 //#########################        OUTPUT        ###############################
-input wire uart_txd_in;
-output logic uart_rxd_out;
-wire tx_ready;
-logic tx_valid;
-logic [7:0] tx_data;
 
 uart_driver UART(.clk(uartclk), .rst(rst), // reset with rest of system
                 .rx_data(), .rx_valid(), // no rx functionality
@@ -99,20 +205,32 @@ uart_driver UART(.clk(uartclk), .rst(rst), // reset with rest of system
                 .uart_tx(uart_rxd_out), .uart_rx(uart_txd_in)
 );
 
-always_ff @(posedge eth_rx_clk) begin : UART_Transmit // run "make usb"
-    if(rst) begin
-        read_pointer<=0;
-    end
-    if(tx_ready) begin // TODO: add some sort of error detection for write/read
-        tx_valid<=1;
-        if(circle_buffer[read_pointer]) begin
-            tx_data<=8'd49; // 1
-        end else begin
-            tx_data<=8'd48; // 0
-        end
-        read_pointer <= read_pointer + 1;
-    end else begin
+always_ff @(posedge uartclk) begin : UART_Transmit // run "make usb"
+    if(rst|uart_start) begin
+        uart_addr<=0;
+        uartbitcounter<=0;
+        read_buffer<=0;
+        tx_data<=0;
         tx_valid<=0;
+    end else begin
+        if(eth_state==U_TX & tx_ready & ~tx_valid) begin // TODO: add some sort of error detection for write/read
+            tx_valid<=1;
+            // if(circle_buffer[read_pointer]) begin
+            if(read_buffer[0]) begin
+                tx_data<=8'd49; // 1
+            end else begin
+                tx_data<=8'd48; // 0
+            end
+            uartbitcounter<=uartbitcounter+1; //this is also one addr desynced
+            if(&uartbitcounter) begin
+                uart_addr<=uart_addr+1;
+                read_buffer<=rd_data;
+            end else begin
+                read_buffer[30:0]<=read_buffer[31:1];
+            end
+        end else begin
+            tx_valid<=0;
+        end
     end
 end
 
@@ -131,7 +249,7 @@ logic [2:0] global_led_pwm;
 
 always_comb begin : LED_drivers
     if(&global_led_pwm) begin
-        if(|sw) begin
+        if(sw[3]) begin
             // 0 - Yellow for RX valid
             led0_b = 0;
             led0_g = eth_rx_dv;
@@ -151,6 +269,26 @@ always_comb begin : LED_drivers
             led2_b = eth_crs;
             led2_g = 0;
             led2_r = eth_crs;
+        end else if(sw[2]) begin
+            // 0 - Green for IDLE
+            led0_b = 0;
+            led0_g = eth_state==E_IDLE;
+            led0_r = 0;
+
+            // 1 - Yellow for E_REC
+            led1_b = 0;
+            led1_g = eth_state==E_REC;
+            led1_r = eth_state==E_REC;
+
+            // 2 - Red for U_TX
+            led2_b = 0;
+            led2_g = 0;
+            led2_r = eth_state==U_TX;
+
+            // 3 - Pink for uart_addr
+            led3_b = uart_addr[6];
+            led3_g = 0;
+            led3_r = uart_addr[6];
         end else begin
             // 0 - White on reset
             led0_b = rst;
